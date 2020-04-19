e3a58db0079831baccdb01e3725c54e95f1d2669
Benyamin Ginzburg <benyomin.94@gmail.com>
2020-04-19 22:30:02 +0300

    init

--- broadcast/main.py
+++ broadcast/main.py
@@ -39,43 +39,14 @@ scheduler.add_job(safe_check, trigger=trigger)
 
 
 if __name__ == '__main__':
-    msg = '''ERROR:omer_broadcast:Forbidden: bot was blocked by the user
-<code>Traceback (most recent call last):
-  File &quot;/home/app/broadcast/job.py&quot;, line 64, in check_time
-    notificate_user(conn, user)
-  File &quot;/home/app/broadcast/helpers.py&quot;, line 79, in notificate_user
-    bot.send_message(user.user_id, msg, parse_mode=&#x27;HTML&#x27;)
-  File &quot;&lt;decorator-gen-2&gt;&quot;, line 2, in send_message
-  File &quot;/root/.local/share/virtualenvs/app-4uwEKS64/lib/python3.7/site-packages/telegram/bot.py&quot;, line 70, in decorator
-    result = func(*args, **kwargs)
-  File &quot;/root/.local/share/virtualenvs/app-4uwEKS64/lib/python3.7/site-packages/telegram/bot.py&quot;, line 351, in send_message
-    timeout=timeout, **kwargs)
-  File &quot;/root/.local/share/virtualenvs/app-4uwEKS64/lib/python3.7/site-packages/telegram/bot.py&quot;, line 178, in _message
-    result = self._request.post(url, data, timeout=timeout)
-  File &quot;/root/.local/share/virtualenvs/app-4uwEKS64/lib/python3.7/site-packages/telegram/utils/request.py&quot;, line 334, in post
-    **urlopen_kwargs)
-  File &quot;/root/.local/share/virtualenvs/app-4uwEKS64/lib/python3.7/site-packages/telegram/utils/request.py&quot;, line 243, in _request_wrapper
-    raise Unauthorized(message)
-telegram.error.Unauthorized: Forbidden: bot was blocked by the user
+    safe_set_time(reset_status=False)
 
-===========================
+    scheduler.start()
+    logger.info('Starting scheduler...')
 
-61
-62                if dt.fromisoformat(user.dt) < now and not user.sent:
-63                    try:
-64 →                      notificate_user(conn, user)
-65                    except Exception as e:
-66                        logger.exception(e)
-</code>'''
-    logger.error(msg)
-    # safe_set_time(reset_status=False)
-    #
-    # scheduler.start()
-    # logger.info('Starting scheduler...')
-    #
-    # try:
-    #     while True:
-    #         sleep(10)
-    # except (KeyboardInterrupt, SystemExit):
-    #     scheduler.shutdown()
+    try:
+        while True:
+            sleep(10)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
 
